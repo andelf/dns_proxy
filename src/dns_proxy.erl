1d2b94238fdf678c705286be0ab94ec428c2e483
Andelf <andelf@gmail.com>
2013-05-08 11:59:45 +0800

    add stat/0 api, move all api func to dns_proxy.

--- src/dns_proxy.erl
+++ src/dns_proxy.erl
@@ -3,12 +3,11 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1, start/0]).
+-export([start/2, stop/1, start/0, sync/0, stat/0]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
-
 start(_StartType, _StartArgs) ->
     dns_proxy_sup:start_link().
 
@@ -18,3 +17,12 @@ stop(_State) ->
 %% start()
 start() ->
     application:start(?MODULE).
+
+%% ===================================================================
+%% Other APIs
+%% ===================================================================
+sync() ->
+    dns_proxy_srv:sync().
+
+stat() ->
+    dns_proxy_srv:stat().
--- src/dns_proxy_srv.erl
+++ src/dns_proxy_srv.erl
@@ -11,7 +11,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, sync/0]).
+-export([start_link/0, sync/0, stat/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -67,6 +67,8 @@ start_link() ->
 sync() ->
     gen_server:call(?MODULE, {dump_db}).
 
+stat() ->
+    ets:info(?RESOLVE_TABLE).
 
 %%%===================================================================
 %%% gen_server callbacks
